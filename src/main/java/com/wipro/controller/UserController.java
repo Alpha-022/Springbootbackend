029bd0e6933f779972053f8521373e4fab6356ac
Saurav Bansode <104521086+Alpha-022@users.noreply.github.com>
2025-03-21 10:04:05 +0530

    Add files via upload

---
+++ src/main/java/com/wipro/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.wipro.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.wipro.model.User;
+import com.wipro.service.UserService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return userService.registerUser(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{email}")
+    public User getUserByEmail(@PathVariable String email) {
+        return userService.findByEmail(email);
+    }
+}
